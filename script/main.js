666bb69f75fbf31e6ef814ec99268f87eb7d6434
Ayushka Partohap <partohap.ayushka@gmail.com>
2017-09-29 15:57:38 +0700

    Rewritten completely and mark as second version. Features: - Game loop - Game objects - Snake growth - Fruit consumption
    Not implemented:
    - Game conditions
    - Fruit randomization
    - Speed alteration
    - Score
    - Final styling

--- script/main.js
+++ script/main.js
@@ -1,84 +1,197 @@
-class Coordinate {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class Placeable {
-    constructor(positions = [[0, 0]]) {
-        this.positions = positions;
-    }
-}
-
-class Snake extends Placeable {
-    constructor(positions = [[20, 20]]) {
-        super(positions);
-        this.direction = 'r';
-        this.token = 'O';
-    }
-}
-
-class Grid {
-    constructor(width = 40, height = 40) {
-        this.width = width;
-        this.height = height;
-        this.positions = Array(height).fill('').map(() => Array(width).fill(' '));
-    }
-
-    place(placeable) {
-        placeable.positions.forEach(
-            function (pos) {
-                this.positions[pos[1]][pos[0]] = placeable.token;
-            },
-            this
+class Grid extends Array {
+  constructor(width = 4, height = 4) {
+    super(height)
+      .fill('')
+        .forEach(
+          (val, index) => {
+            this[index] = Array(width).fill(' ');
+          }
         );
-    }
+  }
+
+  clear() {
+    this.forEach(
+      (r) => {
+        r.fill(' ');
+      }
+    );
+  }
+
+  place(thing) {
+    thing.positions.forEach(
+      (posArr, index) => {
+        if(posArr) {
+          let [x, y] = posArr;
+          this[y][x] = thing.tokens[index];
+        }
+      }
+    );
+  }
+
+  toHTML() {
+    return this.map(
+      (row, yIn) => {
+        let rowHTML = row
+          .map( (cell, xIn) => `<div class="cell ${cell}" id="${yIn}-${xIn}"></div>` )
+            .join('\n');
+
+        return `<div class="row" id="row-${yIn}">\n${rowHTML}\n</div>`;
+      }
+    ).join('\n');
+  }
 }
 
-class GridUI {
-    constructor(width, height) {
-        this.grid = new Grid(width, height);
+class Snake {
+  constructor(positions = [[20, 20]], direction = 'l') {
+    this.positions = positions;
+    this.tokens = Array(this.positions.length).fill('snake');
+    this.direction = direction;
+  }
+
+  head() {
+    return this.positions[0];
+  }
+  
+  tail() {
+    return this.positions[this.positions.length - 1];
+  }
+
+  grow() {
+    this.positions.push(this.tail().map( v => v ));
+    this.tokens.push('snake');
+  }
+
+  update() {
+    let [x, y] = this.head();
+
+    switch(this.direction) {
+      case 'r': 
+        this.positions.unshift([(x + 1), y]);
+        this.tokens.unshift('snake');
+        break;
+
+      case 'l': 
+        this.positions.unshift([(x - 1), y]);
+        this.tokens.unshift('snake');
+        break;
+
+      case 'u':
+        this.positions.unshift([x, (y - 1)]);
+        this.tokens.unshift('snake');
+        break;
+
+      case 'd':
+        this.positions.unshift([x, (y + 1)]);
+        this.tokens.unshift('snake');
+        break;
+
+      default:
+        let [xTemp, yTemp] = this.head();
+        this.positions[0] = [yTemp, xTemp];
+        break;
     }
 
-    toHTML() {
-        return this.grid.positions.map(
-            (rowArr, rIndex) => {
-                let rowHTML = rowArr.map(
-                    (cell, cIndex) => {
-                        return `<div class="cell" id="${rIndex}-${cIndex}">${cell}</div>`;
-                    }
-                ).join('');
-                return `<div class="row" id="row-${rIndex}">${rowHTML}</div>`;
-            }
-        ).join('');
-    }
+    this.positions.pop();
+    this.tokens.pop();
+  }
 }
 
-const render = function render(gridUI) {
-    document
-        .getElementById('board')
-        .insertAdjacentHTML('afterbegin', gridUI.toHTML());
-};
+class Fruit {
+  constructor(positions = [[10, 10]]) {
+    this.positions = positions;
+    this.tokens = Array(this.positions.length).fill('fruit');
+  }
 
-const isControl = function isControl(key) {
-    return ['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft'].includes(key);
-};
-
-const handleControls = function handleControls(event) {
-    console.log(event);
-    let key = event.key;
-    if(isControl(key)) event.preventDefault();
-};
-
+  location() {
+    return this.positions[0];
+  }
+}
 
 document.addEventListener(
-    'DOMContentLoaded',
-    function () {
-        let gridUI = new GridUI(40, 40);
-        let snake = new Snake([[20, 20], [21, 20]]);
-        gridUI.grid.place(snake);
-        render(gridUI);
-        window.addEventListener('keydown', handleControls);
-    }
-);
+  'DOMContentLoaded',
+  () => {
+    let board = document.getElementById('board');
+    let width = 40;
+    let height = 40;
+    let keyJammed = false;
+    let grid = new Grid(width, height);
+    let snake = new Snake([[20, 10], [21, 10]]);
+    let fruit = new Fruit([[
+                          Math.floor(Math.random() * width), 
+                          Math.floor(Math.random() * height)
+                          ]]);
+    let renderIID;
+
+    grid.clear();
+    grid.place(fruit);
+    grid.place(snake);
+    board.innerHTML = grid.toHTML();
+
+    board.addEventListener('click',
+      () => {
+        if(!!renderIID) {
+          clearInterval(renderIID);
+          renderIID = undefined;
+        } else {
+          renderIID = setInterval(
+            () => {
+              let [snakeX, snakeY] = snake.head();
+              let [fruitX, fruitY] = fruit.location();
+
+              snake.update();
+              keyJammed = false;
+
+              if( (snakeX == fruitX)
+                  && (snakeY == fruitY)
+                ) {
+                snake.grow();
+                fruit = new Fruit([[
+                                  Math.floor(Math.random() * width), 
+                                  Math.floor(Math.random() * height)
+                                  ]]);
+              }
+
+              grid.clear();
+              grid.place(fruit);
+              grid.place(snake);
+
+              board.innerHTML = grid.toHTML();
+            },
+            100
+          );
+        };
+      }
+    );
+
+    window.addEventListener('keydown',
+      (event) => {
+        if(!keyJammed) {
+          switch(event.key) {
+            case "ArrowUp":
+              keyJammed = true;
+              event.preventDefault();
+              if(!['u', 'd'].includes(snake.direction)) snake.direction = 'u';
+              break;
+            case "ArrowRight":
+              keyJammed = true;
+              event.preventDefault();
+              if(!['r', 'l'].includes(snake.direction)) snake.direction = 'r';
+              break;
+            case "ArrowLeft":
+              keyJammed = true;
+              event.preventDefault();
+              if(!['r', 'l'].includes(snake.direction)) snake.direction = 'l';
+              break;
+            case "ArrowDown":
+              keyJammed = true;
+              event.preventDefault();
+              if(!['u', 'd'].includes(snake.direction)) snake.direction = 'd';
+              break;
+            default: 
+              break;
+          }
+        }
+      }
+    );
+  }
+)
